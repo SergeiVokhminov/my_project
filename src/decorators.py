3977c26bee981ca526ffd20230f14df522aad393
sergei_vokhminov <rreapeer@list.ru>
2024-07-26 05:45:03 +0300

    update file coverage, module decorators

--- src/decorators.py
+++ src/decorators.py
@@ -3,32 +3,30 @@ from functools import wraps
 
 
 def log(filename: Any) -> Callable:
-    """Декоратор, который логирует начало и конец работы """
+    """Декоратор, который логирует начало или конец работы."""
 
     def decorator(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
                 result = func(*args, **kwargs)
-                log_message = f'{func.__name__} вызывается с аргументами: {args} {kwargs}, {result}'
-                print('my_function OK')
+                log_message = f'{func.__name__} OK. Результат: {result}'
+                with open(filename, "a", encoding='utf-8') as f:
+                    f.write(log_message + '\n')
+                print(log_message)
             except Exception as f:
-                log_message = f'{func.__name__} error: {f}.'
-                print(f'my_function error: {f}. Inputs:{args}, {kwargs}\n')
-            if filename:
-                with open(filename, 'a', encoding='utf-8') as f:
-                    f.write(f'{log_message}\n')
-            else:
-                print(f'{log_message}\n')
-            return result
+                error_message = f"{func.__name__} error: {f}. Inputs:{args}, {kwargs}"
+                with open(filename, "a", encoding='utf-8') as f:
+                    f.write(error_message + '\n')
+                print(error_message)
         return wrapper
     return decorator
 
 
-@log(filename='mylog.txt')
-def my_funcs(x: int, y: int) -> int:
-    return x + y
+@log(filename='../mylog.txt')
+def my_function(x, y):
+    return int(x / y)
 
 
-# my_funcs(1, 6)
-print(my_funcs(1, 6))
+my_function(10, 0)
+# print(my_function(10, 0))
