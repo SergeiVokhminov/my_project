e9fa9684d10e4b611b71164d941e8fa5b108caa7
sergei_vokhminov <rreapeer@list.ru>
2024-07-26 06:15:50 +0300

    update module decorators

--- src/decorators.py
+++ src/decorators.py
@@ -1,32 +1,34 @@
-from typing import Any, Callable
 from functools import wraps
+from typing import Any, Callable
 
 
 def log(filename: Any) -> Callable:
-    """Декоратор, который логирует начало или конец работы."""
+    """Декоратор, который логирует начало или конец работы функции и записывает в файл."""
 
     def decorator(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
                 result = func(*args, **kwargs)
-                log_message = f'{func.__name__} OK. Результат: {result}'
-                with open(filename, "a", encoding='utf-8') as f:
-                    f.write(log_message + '\n')
+                log_message = f"{func.__name__} OK. Результат: {result}"
+                with open(filename, "a", encoding="utf-8") as f:
+                    f.write(log_message + "\n")
                 print(log_message)
             except Exception as f:
                 error_message = f"{func.__name__} error: {f}. Inputs:{args}, {kwargs}"
-                with open(filename, "a", encoding='utf-8') as f:
-                    f.write(error_message + '\n')
+                with open(filename, "a", encoding="utf-8") as f:
+                    f.write(error_message + "\n")
                 print(error_message)
+
         return wrapper
+
     return decorator
 
 
-@log(filename='../mylog.txt')
+@log(filename="../mylog.txt")
 def my_function(x, y):
     return int(x / y)
 
 
-my_function(10, 0)
+my_function(10, 2)
 # print(my_function(10, 0))
