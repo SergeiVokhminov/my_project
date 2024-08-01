cbad104e4815f7f86d8a6008472448536208014e
sergei_vokhminov <rreapeer@list.ru>
2024-08-02 00:15:11 +0300

    add module utils

---
+++ src/utils.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+
+PATH_TO_FILE = os.path.join(os.path.dirname(__file__), "..", "data", "operations.json")
+
+
+def get_json_file(path: str) -> list:
+    """Функция, которая принимает JSON-файл и возвращает список словарей с данными о финансовых транзакциях."""
+    try:
+        with open(path, "r", encoding="utf-8") as f:
+            try:
+                data = json.load(f)
+                return data
+            except json.JSONDecodeError:
+                return []
+    except FileNotFoundError:
+        return []
+
+
+if __name__ == "__main__":
+    print(get_json_file(PATH_TO_FILE))
