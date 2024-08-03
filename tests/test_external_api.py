cdaf3bc7240093302c35874727f939dc0807a9c0
sergei_vokhminov <rreapeer@list.ru>
2024-08-03 11:05:41 +0300

    add module test_external_api

---
+++ tests/test_external_api.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch
+
+from src.external_api import fnc_convert_rud
+
+
+@patch("requests.get")
+def test_fnc_convert_rub(mock_test):
+    transaction = {'amount': 200, 'currency': 'RUB'}
+    mock_test.return_value.json.return_value = {'result': 200}
+    assert fnc_convert_rud(transaction) == 200
+
+
+@patch("requests.get")
+def test_fnc_convert_usd(mock_test):
+    transaction = {'amount': 100, 'currency': 'USD'}
+    mock_test.return_value.json.return_value = {'result': 12312.21}
+    assert fnc_convert_rud(transaction) == 12312.21
+
+
+@patch("requests.get")
+def test_fnc_convert_eur(mock_test):
+    transaction = {'amount': 100, 'currency': 'EUR'}
+    mock_test.return_value.json.return_value = {'result': 15321.33}
+    assert fnc_convert_rud(transaction) == 15321.33
