af8782ca97214316b675e26a9c78d5f5229bd002
sergei_vokhminov <rreapeer@list.ru>
2024-08-05 01:33:03 +0300

    update module masks, utils

--- src/masks.py
+++ src/masks.py
@@ -1,16 +1,45 @@
-def get_mask_card_number(card_number: str) -> str:
-    """Функция, которая маскирует номер карты."""
+import logging
 
+my_logger = logging.getLogger("masks")
+file_handler = logging.FileHandler("../logs/masks.log", "w", encoding="utf-8")
+file_formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formater)
+my_logger.addHandler(file_handler)
+my_logger.setLevel(logging.INFO)
+
+
+def get_mask_card_number(card_number: str) -> str:
+    """
+    Функция маскирует номер карты.
+    :param card_number: Получает номер карты.
+    :return: Маскирует номер карты.
+    """
+
+    my_logger.info(f"Начало работы функции, получен номер карты: {card_number}")
     if card_number.isdigit() and len(card_number) == 16:
+        my_logger.info("Номер указанной карты замаскирован ")
         return f"{card_number[:4]} {card_number[4:6]}** {'*' * 4} {card_number[-4:]}"
     else:
+        my_logger.error("Указан неверный номер карты")
         return "Неверный номер карты"
 
 
 def get_mask_account(account_number: str) -> str:
-    """Функция, которая маскирует номер счета."""
+    """
+    Функция маскирует номер счета.
+    :param account_number: Получает номер счета.
+    :return: Замаскированный счет.
+    """
 
+    my_logger.info(f"Начало работы функции, получен номер cчёта: {account_number}")
     if account_number.isdigit() and len(account_number) == 20:
+        my_logger.info("Номер указанного счёта замаскирован")
         return f"**{account_number[-4:]}"
     else:
-        return "Неверный номер счета"
+        my_logger.error("Указан неверный номер счёта")
+        return "Неверный номер счёта"
+
+
+if __name__ == "__main__":
+    print(get_mask_card_number("6831982476737650е"))
+    print(get_mask_account("68319824767376581234щ"))
--- src/utils.py
+++ src/utils.py
@@ -1,23 +1,37 @@
 import json
+import logging
 import os
 from typing import Any
 
-PATH_TO_FILE = os.path.join(os.path.dirname(__file__), "..", "data", "operations.json")
+my_logger = logging.getLogger("utils")
+file_handler = logging.FileHandler("logs/utils.log", "w", encoding="utf-8")
+file_formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formater)
+my_logger.addHandler(file_handler)
+my_logger.setLevel(logging.INFO)
+
+PATH_TO_FILE = os.path.join(os.path.dirname(__file__), "..", "data", "operation.json")
 
 
 def get_json_file(path: str) -> Any:
-    """Функция, которая принимает JSON-файл и возвращает список словарей с данными о финансовых транзакциях."""
+    """
+    Функция принимает JSON-файл и возвращает список словарей с данными о финансовых транзакциях.
+    :param path: Получает JSON-файл.
+    :return: Возвращает список словарей с данными о финансовых транзакциях.
+    """
+
     try:
         with open(path, "r", encoding="utf-8") as f:
             try:
+                my_logger.info("Открытие файла")
                 data = json.load(f)
                 return data
             except json.JSONDecodeError:
-                data = []
-                return data
+                my_logger.error("Неверный формат файла")
+                return []
     except FileNotFoundError:
-        data = []
-        return data
+        my_logger.error("Файл не найден")
+        return []
 
 
 if __name__ == "__main__":
