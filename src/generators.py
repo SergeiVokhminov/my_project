748514ab6c0aab768784ea2514c307b7477fea8f
sergei_vokhminov <rreapeer@list.ru>
2024-08-12 01:43:21 +0300

    update module generators

--- src/generators.py
+++ src/generators.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
             "id": 939719570,
             "state": "EXECUTED",
             "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "RUB", "code": "RUB"}},
             "description": "Перевод организации",
             "from": "Счет 75106830613657916952",
             "to": "Счет 11776614605963066702",
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     ]
 
     usd_transactions = filter_by_currency(transactions, "USD")
-    for _ in range(2):
+    for _ in range(1):
         print(next(usd_transactions))
     print()
     descriptions = transaction_descriptions(transactions)
